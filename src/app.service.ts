ef448b07ac293a331e92a14c587b9986dbbf0889
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-14 20:49:38 +0200

    impl service behaviour to list dir / upload catalog / download catalog

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { FtpService } from 'nestjs-ftp';
+import { FileInfo, FTPResponse } from 'basic-ftp';
 
 @Injectable()
-export class AppService {}
+export class AppService {
+  constructor(private readonly ftpService: FtpService) {}
+
+  async listFiles(): Promise<FileInfo[]> {
+    try {
+      const filesInfos = await this.ftpService.list();
+      return filesInfos;
+    } catch (error) {
+      throw new InternalServerErrorException(`Error: ${error.message}`);
+    }
+  }
+
+  async uploadFile(fileName: string, remotePath: string): Promise<FTPResponse> {
+    try {
+      return await this.ftpService.upload(fileName, remotePath);
+    } catch (error) {
+      throw new InternalServerErrorException(`Error: ${error.message}`);
+    }
+  }
+
+  async downloadFile(
+    localPath: string,
+    remotePath: string,
+  ): Promise<FTPResponse> {
+    try {
+      return await this.ftpService.downloadTo(localPath, remotePath);
+    } catch (error) {
+      throw new InternalServerErrorException(`Error: ${error.message}`);
+    }
+  }
+}
