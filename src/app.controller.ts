b8c866c896f90d261674cc4a8f93a566850e175a
Julien Nicolas <julien.brcht.rndlt@gmail.com>
2021-10-14 20:49:06 +0200

    impl routes for list/upload/download on ftp server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInfo, FTPResponse } from 'basic-ftp';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+  @Get('htdocs')
+  getFiles(): Promise<FileInfo[]> {
+    return this.appService.listFiles();
+  }
+
+  @Get('catalog')
+  downloadFile(@Query() query): Promise<FTPResponse> {
+    const { localPath, remotePath } = query;
+    return this.appService.downloadFile(localPath, remotePath);
+  }
+
+  @Post('catalog')
+  uploadFile(
+    @Body() /* body  */
+    { filePath, remotePath }: { filePath: string; remotePath: string },
+  ): Promise<FTPResponse> {
+    //const { filePath, remotePath } = body;
+    return this.appService.uploadFile(filePath, remotePath);
+  }
 }
